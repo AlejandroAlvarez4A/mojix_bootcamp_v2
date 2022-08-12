721d9d6de62aea7d87cd1a1f0274e0b706dce178
Alejandro <a.alejandro.alvarez.a@gmail.com>
2022-08-11 20:08:48 -0400

    add documentation for walrus operator

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -4,7 +4,7 @@ st.write("Hello from the mojix bootcamp")
 
 st.write("""
 # 1. Walrus operator
-The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
+The Walrus or **:=** `operator` is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
 """)
 
 code = '''def hello():
