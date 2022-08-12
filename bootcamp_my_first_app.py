0f6265d2d005cb5818d3fc0a834ff3d472b0bab7
Alejandro <a.alejandro.alvarez.a@gmail.com>
2022-08-11 20:07:24 -0400

    Add walrus operator

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.write("Hello from the mojix bootcamp")
+
+st.write("""
+# 1. Walrus operator
+The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
+""")
+
+code = '''def hello():
+     print("Hello, Streamlit!")'''
+
+st.code(code, language='python')
