06558d296cc72c258ebe9c276b9de969fb2e3b2e
Alejandro <a.alejandro.alvarez.a@gmail.com>
2022-08-11 20:34:02 -0400

    add reversing

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import walrus 
 import splitting
+import reversing
 import mergingTwoDictionaries
 
 walrus.walrus()
 splitting.splitting()
+reversing.reversing()
 mergingTwoDictionaries.merging()
 
