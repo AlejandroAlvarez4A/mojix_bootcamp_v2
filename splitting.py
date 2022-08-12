d9073ce0eb92a549edd3c8754225137e30a0c6c7
Alejandro <a.alejandro.alvarez.a@gmail.com>
2022-08-11 20:29:15 -0400

    add description in splitting a string

--- splitting.py
+++ splitting.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 def splitting():
   st.write("""
-  ## 2. If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the string operations a lot easier!
+  ## 2. Splitting a string
+  If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the string operations a lot easier!
   """)
 
   st.write("""
