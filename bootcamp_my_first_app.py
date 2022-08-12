0d47e0d809773951f3fcc376e158da23e075d267
Alejandro <a.alejandro.alvarez.a@gmail.com>
2022-08-11 20:11:24 -0400

    add code for walrus operator

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -3,11 +3,28 @@ import streamlit as st
 st.write("Hello from the mojix bootcamp")
 
 st.write("""
-# 1. Walrus operator
-The Walrus or **:=** `operator` is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
+## 1. Walrus operator
+The Walrus or **:=** operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
 """)
 
-code = '''def hello():
-     print("Hello, Streamlit!")'''
+st.write("""
+### Example
+If we want to check and print the length of a list:
+""")
 
+code = '''
+Mylist = [1,2,3]
+if(l := len(mylist) > 2)
+print(l)
+'''
+st.code(code, language='python')
+
+st.write("""
+### Output
+If we want to check and print the length of a list:
+""")
+
+code = '''
+3
+'''
 st.code(code, language='python')
